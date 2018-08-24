5f8b15da2da995cc26e9092e9e22eeb7a09985a9
David Monllao <david.monllao@gmail.com>
2018-08-24 16:37:02 +0200

    Remove reg/no-reg models

--- models.py
+++ models.py
@@ -6,104 +6,71 @@ def get_all():
 
     models.append({
         'name': 'No context NN - 1 hidden.',
-        'network': network.fc_1h,
-        'feature_set': 'nocontext'
-    })
-
-    models.append({
-        'name': 'With peers NN - 1 hidden.',
-        'network': network.fc_1h,
-        'feature_set': 'withpeers'
-    })
-
-    models.append({
-        'name': 'All features NN - 1 hidden.',
-        'network': network.fc_1h,
-        'feature_set': 'all'
-    })
-
-    models.append({
-        'name': 'No context NN - 2 hidden.',
-        'network': network.fc_2h,
-        'feature_set': 'nocontext'
+        'network': network.fc,
+        'feature_set': 'nocontext',
+        'options': {'n_layers': 1},
     })
 
     models.append({
         'name': 'With peers NN - 2 hidden.',
-        'network': network.fc_2h,
-        'feature_set': 'withpeers'
+        'network': network.fc,
+        'feature_set': 'withpeers',
+        'options': {'n_layers': 2},
     })
 
     models.append({
         'name': 'All features NN - 2 hidden.',
-        'network': network.fc_2h,
-        'feature_set': 'all'
+        'network': network.fc,
+        'feature_set': 'all',
+        'options': {'n_layers': 2},
     })
 
     models.append({
         'name': 'Activity features trained with context.',
         'network': network.inctx,
-        'feature_set': 'all'
-    })
-
-    models.append({
-        'name': 'Activity features trained with context (REG).',
-        'network': network.inctx_reg,
-        'feature_set': 'all'
+        'feature_set': 'all',
+        'options': {'reg': False},
     })
 
     models.append({
         'name': 'Activity features trained with context + original activity features.',
         'network': network.inctx_extra,
-        'feature_set': 'all'
-    })
-
-    models.append({
-        'name': 'Activity features trained with context + original activity features (REG).',
-        'network': network.inctx_extra_reg,
-        'feature_set': 'all'
-    })
-
-    models.append({
-        'name': 'Separate no-context / 1 context inputs.',
-        'network': network.simple_separate_1,
-        'feature_set': 'all'
+        'feature_set': 'all',
+        'options': {'reg': False},
     })
 
     models.append({
         'name': 'Separate no-context / 1 context inputs (REG).',
-        'network': network.simple_separate_1_reg,
-        'feature_set': 'all'
+        'network': network.simple_separate_1,
+        'feature_set': 'all',
+        'options': {'reg': True},
     })
 
     models.append({
         'name': 'Separate no-context / all context inputs.',
         'network': network.simple_separate_all,
-        'feature_set': 'all'
-    })
-
-    models.append({
-        'name': 'Separate no-context / all context inputs (REG).',
-        'network': network.simple_separate_all_reg,
-        'feature_set': 'all'
+        'feature_set': 'all',
+        'options': {'reg': False},
     })
 
     models.append({
         'name': 'Separate activity / course peers / required.',
         'network': network.complex_separate,
-        'feature_set': 'all'
+        'feature_set': 'all',
     })
 
     #models.append({
         #'name': 'Combinations of features in pairs.',
         #'network': network.comb,
-        #'feature_set': 'all'
+        #'feature_set': 'all',
+        #'options': {'reg': False},
     #})
 
     #models.append({
         #'name': 'Combinations of features in pairs (REG).',
-        #'network': network.comb_reg,
-        #'feature_set': 'all'
+        #'network': network.comb,
+        #'feature_set': 'all',
+        #'options': {'reg': True},
     #})
 
     return models
